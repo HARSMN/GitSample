b37eae0aa9220f530409579a2741c35a3c4b94c2
HARSMN <105035961+HARSMN@users.noreply.github.com>
2022-05-09 10:58:23 +0530

    Update Emp.java

--- src/org/sample/Emp.java
+++ src/org/sample/Emp.java
@@ -4,12 +4,12 @@ public class Emp {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("First change");
-<<<<<<< HEAD
+	
+
 		System.out.println("Emp-2 new changes");
-=======
+
 		System.out.println("Emp-1 new changes");
->>>>>>> be079ef06a6148d69ceccf808bbe19dd3f33e01d
+
 	}
 
 }
